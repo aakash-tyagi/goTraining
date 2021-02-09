4c098c728fdce4de9a6c633a28e95c272ff7fcb3
tyagiaakash <tyagi01aakash@gmail.com>
2021-02-09 23:50:12 +0530

    web server added

---
+++ web-server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>WELCOME TO WEB APP</h1>")
+}
+
+func handlerFuncInfo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>WELCOME TO WEB APP INFO PAGE</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	http.HandleFunc("/info", handlerFuncInfo)
+	http.ListenAndServe(":3000", nil)
+}
