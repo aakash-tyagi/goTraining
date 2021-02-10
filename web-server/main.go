515de893b143d8aeeb2a9e65b6bdf7a5968b247b
tyagiaakash <tyagi01aakash@gmail.com>
2021-02-10 12:03:53 +0530

    wat up?

--- web-server/main.go
+++ web-server/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>WELCOME TO WEB APP</h1>")
+	fmt.Fprint(w, "<h1>WELCOME  APP</h1>")
 }
 
 func handlerFuncInfo(w http.ResponseWriter, r *http.Request) {
