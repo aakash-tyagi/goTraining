8c7390c04d058b9f86de753934bdf27415c9d7ca
tyagiaakash <tyagi01aakash@gmail.com>
2021-02-12 11:17:14 +0530

    added page not found

--- web-server/main.go
+++ web-server/main.go
@@ -13,6 +13,10 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>WELCOME  APP 1</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "to contact us mail @ <a href = \"akkutyagi.85.2@gmail.com\">Aakash</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>Page not found</h1><p>Hi i am here come to me</p>")
+
 	}
 
 }
@@ -22,7 +26,11 @@ func handlerFuncInfo(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	fmt.Println("some one visted to the page 0")
 	http.HandleFunc("/", handlerFunc)
+	fmt.Println("some one visted to the page 1")
 	http.HandleFunc("/info", handlerFuncInfo)
+	fmt.Println("some one visted to the page 2")
 	http.ListenAndServe(":3000", nil)
+
 }
