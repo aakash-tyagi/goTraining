3b10c3389f7b2e993e55466b0142f44d3bb2f264
aakash <tyagi01aakash.com>
2021-07-05 12:18:18 +0530

    block querying

---
+++ Geth/BalanceQuery/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+
+	client, err := ethclient.Dial("http://localhost:8545")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("njoo")
+
+	account := common.HexToAddress("0x9E07A9DBA2C165640Bb03430BbFeBD178DD2b387")
+
+	balance, err := client.BalanceAt(context.Background(), account, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(balance)
+	blockNumber := big.NewInt(0)
+
+	balance, err = client.BalanceAt(context.Background(), account, blockNumber)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fbalance := new(big.Float)
+	fbalance.SetString(balance.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
+	fmt.Println(ethValue)
+
+	fmt.Println(balance)
+
+}
