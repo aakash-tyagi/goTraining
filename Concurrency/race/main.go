0eabd62cb8c6d2de5dbb319852364d05f430ae0d
aakash <tyagi01aakash.com>
2021-09-18 13:56:54 +0530

    creating race condition using mutex

---
+++ Concurrency/race/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func main() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("GoRoutines:", runtime.NumGoroutine())
+
+	counter := 0
+
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := counter
+
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+		fmt.Println("GoRoutines:", runtime.NumGoroutine())
+	}
+
+	wg.Wait()
+	fmt.Println("GoRoutines:", runtime.NumGoroutine())
+	fmt.Println("count:", counter)
+}
